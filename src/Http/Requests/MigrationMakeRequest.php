3ada5e8d6108f6cbd80313a973e4024638227561
anvar <anvarindastries@gmail.com>
2024-08-08 15:55:03 +0500

    Migration create feature added

---
+++ src/Http/Requests/MigrationMakeRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Uzinfocom\LaravelGenerator\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MigrationMakeRequest extends FormRequest {
+    public function authorize(): bool {
+        return true;
+    }
+
+    public function rules(): array {
+        return [
+            'name' => 'required|string|between:2,255',
+            'columns.*.name' => 'required|string',
+            'columns.*.type' => 'required|string',
+            'namespace' => 'string|nullable'
+        ];
+    }
+}
