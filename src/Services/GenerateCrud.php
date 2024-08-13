19fd851fe46583a4bb18a11b5a269639eab5cbb4
azizbek97 <azizgulaliyev44@gmail.com>
2024-08-13 09:26:45 +0500

    advanced crud

--- src/Services/GenerateCrud.php
+++ src/Services/GenerateCrud.php
@@ -3,7 +3,6 @@
 namespace Uzinfocom\LaravelGenerator\Services;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class GenerateCrud extends AllGenerator {
@@ -14,30 +13,16 @@ class GenerateCrud extends AllGenerator {
     }
 
     public function generate(array $form): void {
+        $stub = $this->getStub();
         $modelName = Str::afterLast($form['model'], '\\');
-        $modelInfo = ['name' => $modelName,'namespace' => $form['model']];
+        $modelInfo = ['name' => $modelName, 'namespace' => $form['model']];
         $namespace = Str::beforeLast(($form['controllerPrefix'] . $form['controllerName']), '\\');
         $controllerName = Str::afterLast($form['controllerName'], '\\') . $form['controllerSuffix'];
 
-        // create request
-        $createRequest = Str::afterLast($form['createRequestName'], '\\') . $form['createRequestSuffix'];
-        $useCreateRequest = Str::beforeLast($form['createRequestName'], '\\') . '\\' . $createRequest;
-        $this->requestCreateGenerate($form, $modelInfo);
-
-        // update request
-        $updateRequest = Str::afterLast($form['updateRequestName'], '\\') . $form['updateRequestSuffix'];
-        $useUpdateRequest = Str::beforeLast($form['updateRequestName'], '\\') . '\\' . $updateRequest;
-        $this->requestUpdateGenerate($form, $modelInfo);
-
-        // service
-        $serviceName = Str::afterLast($form['serviceName'], '\\') . $form['serviceSuffix'];
-        $useService = Str::beforeLast($form['serviceName'], '\\') . '\\' . $serviceName;
-        $this->serviceGenerate($form, $modelInfo);
-
-        // resource
-        $resourceName = Str::afterLast($form['resourceName'], '\\') . $form['resourceSuffix'];
-        $useResource = Str::beforeLast($form['resourceName'], '\\') . '\\' . $resourceName;
-        $this->resourceGenerate($form, $modelInfo);
+        $this->requestCreateGenerate($form, $modelInfo, $stub);
+        $this->requestUpdateGenerate($form, $modelInfo, $stub);
+        $this->serviceGenerate($form, $modelInfo, $stub);
+        $this->resourceGenerate($form, $modelInfo, $stub);
 
         $modelNameSingular = Str::lcfirst($modelName);
         $modelNamePlural = Str::plural($modelNameSingular);
@@ -45,48 +30,42 @@ class GenerateCrud extends AllGenerator {
 
         /* @var Model $entity */
         // Read boilerplate from storage
-        $stub = $this->getStub();
 
-        $content = str_replace([
+        $stub = str_replace([
             '{{ namespace }}',
             '{{ controllerName }}',
             '{{ modelName }}',
             '{{ modelNamePlural }}',
             '{{ modelNameSingular }}',
-            '{{ modelResourceName }}',
-            '{{ createRequest }}',
-            '{{ useCreateRequest }}',
-            '{{ updateRequest }}',
-            '{{ useUpdateRequest }}',
-            '{{ serviceName }}',
-            '{{ useService }}',
-            '{{ resourceName }}',
-            '{{ useResource }}'
+            '{{ modelResourceName }}'
         ], [
             $namespace,
             $controllerName,
             $modelName,
             $modelNamePlural,
             $modelNameSingular,
-            $modelResourceName,
-            $createRequest,
-            $useCreateRequest,
-            $updateRequest,
-            $useUpdateRequest,
-            $serviceName,
-            $useService,
-            $resourceName,
-            $useResource
+            $modelResourceName
         ], $stub);
 
         // Make a director if it does not exist
         $location = $this->resolvePath($namespace);
 
         // Make ready boilerplate as Service $namespace/$controllerName
-        $this->make($location, $controllerName, $content);
+        $this->make($location, $controllerName, $stub);
     }
 
-    private function serviceGenerate(array $form, array $modelInfo): void {
+    private function serviceGenerate(array $form, array $modelInfo, &$stub): void {
+        $serviceName = Str::afterLast($form['serviceName'], '\\') . $form['serviceSuffix'];
+        $useService = Str::beforeLast($form['serviceName'], '\\') . '\\' . $serviceName;
+
+        $stub = str_replace([
+            '{{ serviceName }}',
+            '{{ useService }}'
+        ], [
+            $serviceName,
+            $useService
+        ], $stub);
+
         $generator = new ServiceGenerator();
         $generator->generate(
             $modelInfo,
@@ -95,7 +74,18 @@ class GenerateCrud extends AllGenerator {
         );
     }
 
-    private function resourceGenerate(array $form, array $modelInfo): void {
+    private function resourceGenerate(array $form, array $modelInfo, &$stub): void {
+        $resourceName = Str::afterLast($form['resourceName'], '\\') . $form['resourceSuffix'];
+        $useResource = Str::beforeLast($form['resourceName'], '\\') . '\\' . $resourceName;
+
+        $stub = str_replace([
+            '{{ resourceName }}',
+            '{{ useResource }}'
+        ], [
+            $resourceName,
+            $useResource
+        ], $stub);
+
         $generator = new GenerateResource();
         $generator->generate(
             $modelInfo,
@@ -104,7 +94,18 @@ class GenerateCrud extends AllGenerator {
         );
     }
 
-    private function requestCreateGenerate(array $form, array $modelInfo): void {
+    private function requestCreateGenerate(array $form, array $modelInfo, &$stub): void {
+        $createRequest = Str::afterLast($form['createRequestName'], '\\') . $form['createRequestSuffix'];
+        $useCreateRequest = Str::beforeLast($form['createRequestName'], '\\') . '\\' . $createRequest;
+
+        $stub = str_replace([
+            '{{ createRequest }}',
+            '{{ useCreateRequest }}'
+        ], [
+            $createRequest,
+            $useCreateRequest
+        ], $stub);
+
         $generator = new GenerateRequest();
         $generator->generateCreate(
             $modelInfo,
@@ -113,7 +114,18 @@ class GenerateCrud extends AllGenerator {
         );
     }
 
-    private function requestUpdateGenerate(array $form, array $modelInfo): void {
+    private function requestUpdateGenerate(array $form, array $modelInfo, &$stub): void {
+        $updateRequest = Str::afterLast($form['updateRequestName'], '\\') . $form['updateRequestSuffix'];
+        $useUpdateRequest = Str::beforeLast($form['updateRequestName'], '\\') . '\\' . $updateRequest;
+
+        $stub = str_replace([
+            '{{ updateRequest }}',
+            '{{ useUpdateRequest }}'
+        ], [
+            $updateRequest,
+            $useUpdateRequest,
+        ], $stub);
+
         $generator = new GenerateRequest();
         $generator->generateUpdate(
             $modelInfo,
